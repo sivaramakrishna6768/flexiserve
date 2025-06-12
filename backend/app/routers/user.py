b861fd7142c173da8c6fe984ed0251692e778842
siva_ramakrishna <krishpalaparthy6768@gmail.com>
2025-06-12 00:58:48 -0400

    Add message field to create_user response

--- backend/app/routers/user.py
+++ backend/app/routers/user.py
@@ -17,14 +17,13 @@ def get_db():
     finally:
         db.close()
 
-@router.post("/", response_model=dict)
+@router.post("/", response_model=schemas.UserResponse)
 def create_user(user_req: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = models.user.User(name=user_req.name, email=user_req.email)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
-    return {"message": "User created successfully", "user": db_user}
-
+    return db_user
 
 @router.get("/", response_model=list[schemas.UserResponse])
 def get_all_users(db: Session = Depends(get_db)):
